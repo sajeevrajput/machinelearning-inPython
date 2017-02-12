d99fa03fb0c6e07707fb55d6d5f5b0c3b3d90f20
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2017-02-12 16:28:50 +0530

    Update Perceptron.py
    improved documentation

--- Perceptron.py
+++ Perceptron.py
@@ -3,6 +3,14 @@ import numpy as np
 class Perceptron(object):
     """
     Creates and return a Perceptron object
+    __init__(): takes in learning rate and no of iterations(with a default value of 0.01 and 10 respectively) to perform
+                    assigns them to instance variables
+    fit(X,y):   takes in training data set X of shape (no_of_samples,no_of_features) and and corresponding outcome vector
+                    y of shape (no_of_samples,)
+                instance variable theta is the weight vector that would be fit after the iterations
+                returns a Perceptron object
+    predict(xi):takes in a training sample and predicts its outcome
+    
     """
     
     def __init__(self,l_rate=0.01,n_iter=10):
@@ -26,7 +34,7 @@ class Perceptron(object):
                 self.theta[1:]+=update*xi
                 n_err+=np.where(update==0,0,1)
             self.errors_.append(n_err)
-            print(self.errors_)
+            #print(self.errors_)
         return self
     
     
