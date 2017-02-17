20b1a12fc8109e74e1f9a76ecdc06a501d344441
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2017-02-18 00:37:42 +0530

    modified error statement as (y-hypothesis)

--- AdalineSGD.py
+++ AdalineSGD.py
@@ -1,28 +1,37 @@
 import numpy as np
 
 class AdalineSGD(object):
+    
     def __init__(self, l_rate = 0.01, n_iter = 10):
         self.l_rate = l_rate
         self.n_iter = n_iter
+    
     def fit(self, X, y):
         self.avgcost_ = []
         self.weights_ = np.zeros(X.shape[1]+1)
         
         for _ in range(self.n_iter):
+            X,y = self.shuffle(X,y)             #Shuffles Input, output pair in eac iteration to prevent cycles
             cost_iter=[]
+            
             for xi,yi in zip(X, y):
+                error = yi - self.hypothesis(xi)
+                cost_iter.append(0.5 * error**2)
                 
-                error = yi - self.predict(xi)
-                cost_iter.append(0.5 * (error**2).sum())
-                
-                self.weights_[1:] += self.l_rate * error * xi
+                self.weights_[1:] += self.l_rate * error * xi   #Gradient descent by each sample
                 self.weights_[0] += self.l_rate * error
             self.avgcost_.append(sum(cost_iter)/len(y))
         print(self.avgcost_)
-        #print(self.weights_)
         return self
-    def predict(self,xi):
-        return np.where((np.dot(xi,self.weights_[1:]) + self.weights_[0] >= 0), 1, -1)        
+    
+    def hypothesis(self, xi):
+        return np.dot(xi, self.weights_[1:]) + self.weights_[0]
+    
+   #def predict(self,xi):
+   #    return np.where((self.hypothesis>=0),1,-1)
+    def shuffle(self, X, y):
+        r=np.random.permutation(len(y))
+        return X[r], y[r]   
 """
 STARTER CODE
 *************
@@ -43,7 +52,7 @@ for dim in np.arange(X.ndim):
 
 a=AdalineSGD()
 a.fit(X,y)
-plt.plot(np.arange(a.n_iter),a.avgcost_)
+plt.plot(np.arange(a.n_iter),a.avgcost_,marker='o')
 plt.show()
 
 """
