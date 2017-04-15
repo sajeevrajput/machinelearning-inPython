b3bc802cb785094f1dbdc19f9c60becb14ee5580
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2017-04-16 01:21:23 +0530

    added code for heat maps

--- AgglomerativeClustering.py
+++ AgglomerativeClustering.py
@@ -6,21 +6,33 @@ np.random.seed(123)
 X = np.random.random_sample([5,3])*10
 
 #Define labels and variable names
-#variables = ['V1','V2','V2']
-#labels = ['ID0', 'ID1', 'ID2', 'ID3', 'ID4']
+variables = ['V1','V2','V2']
+labels = ['ID0', 'ID1', 'ID2', 'ID3', 'ID4']
 
-#import pandas as pd
-#df = pd.DataFrame(X, columns = variables, index = labels)
+import pandas as pd
+df = pd.DataFrame(X, columns = variables, index = labels)
 
 from scipy.spatial.distance import pdist
-row_dist = pdist(X, metric = 'euclidean') # can use dataframe - df as well
-
+row_dist = pdist(df, metric = 'euclidean') 
 #we can now use the condensed distance matrix - row_dist for agglometric clustering
+
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import linkage, dendrogram
-#Returns linkage matrix. Can use df.values also instead of pairwise distance matrix
-linkage_mat = linkage(row_dist, method = 'complete') 
+linkage_mat = linkage(row_dist, method = 'complete') #Returns linkage matrix. 
+                                                #Can use df.values also instead of pairwise distance matrix
+fig=plt.figure(figsize=[8,8])
+axd=fig.add_axes([0.09,1,0.2,0.6])
+row_dend=dendrogram(linkage_mat, labels=labels,orientation='left')
 
-row_dend=dendrogram(linkage_mat, labels=labels)
-plt.ylabel('Euclidean Distance')
+clustered_X = X[row_dend['leaves'][::-1]]
+
+axm=fig.add_axes([0.22,1,0.6,0.6])
+cax=axm.matshow(clustered_X,cmap="hot_r")
+axd.set_xticks([])
+axd.set_yticks([])
+
+axm.set_xticklabels(['']+variables)
+axm.set_yticklabels(['']+row_dend['ivl'][::-1])
+fig.colorbar(cax)
+
 plt.show()
