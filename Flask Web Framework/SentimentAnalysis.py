4dc3be8a749aa69c056405bb80377a36bfaa9af1
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2017-07-11 01:11:52 +0530

    added validators

--- Flask Web Framework/SentimentAnalysis.py
+++ Flask Web Framework/SentimentAnalysis.py
@@ -1,18 +1,22 @@
-from flask import Flask, render_template
-from wtforms import Form, TextAreaField
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+from flask_wtf import FlaskForm
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 
-class ReviewForm(Form):
-    reviewText = TextAreaField()
+class ReviewForm(FlaskForm):
+    reviewText = TextAreaField("", validators=[validators.DataRequired(),
+                                               validators.length(min=10)])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     reviewform = ReviewForm()
+    if reviewform.validate_on_submit():
+        return "<h1>Form submitted successfully !<h1>"
     return render_template("review.html", form=reviewform)
 
 
